be879a00872396542b82031833669602812a033c
Luis Pérez <luis.perez@hotmart.com>
2020-09-11 19:28:29 -0300

    Componente de Image do Profile

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import ProfileImage from '../Image/Profile';
 import './style.scss';
 
 const Header = () => {
-  return <div className="header">header</div>;
+  return (
+    <div className="header">
+      <ProfileImage src="/img/profile.png" className="header__profile-image" />
+    </div>
+  );
 };
 
 export default Header;
---
+++ src/components/Image/Profile/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import './style.scss';
+
+const avatarURL = '/img/avatar.jpeg';
+
+const ProfileImage = (props) => {
+  // permitir tentar o avatar em caso de falha da imagem de props
+  let errorFlag = true;
+
+  const onError = (e) => {
+    // caso aconteça um erro no em qualquer das imagens não entra em loop
+    if (errorFlag) {
+      errorFlag = false;
+      e.target.src = avatarURL;
+    }
+  };
+
+  const { src, className, ...rest } = props;
+  return (
+    <img
+      src={src}
+      onError={onError}
+      className={classnames('profile-image', className)}
+      {...rest}
+    />
+  );
+};
+
+ProfileImage.propTypes = {
+  src: PropTypes.string,
+};
+
+ProfileImage.defaultProps = {
+  src: '',
+};
+
+export default ProfileImage;
